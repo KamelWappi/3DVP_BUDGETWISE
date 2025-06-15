fc6bfedee5f9a511286ed2ba60a1fcad4d52105e
kamelwappi <wilfriedwappi1@gmail.com>
2025-06-15 23:34:17 +0200

    Correcteion d'un bug

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,8 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
+
+def test_revenus():
+    tester = app.test_client()
+    response = tester.get('/revenus')
+    assert response.status_code == 200
