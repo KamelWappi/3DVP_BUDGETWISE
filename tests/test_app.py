40fc6df40f8c4a9167a109add4578bae72ab4607
kamelwappi <wilfriedwappi1@gmail.com>
2025-06-16 00:37:42 +0200

    Correction d'un bug...

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,8 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/..'))
-from app import app
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from app import app  # noqa: E402
 
 
 def test_home():
